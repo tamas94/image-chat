8736708f30b4b8453c93f0aa701f9c55a3bfdc63
czimbortibor <czimbortibor@gmail.com>
2016-11-16 19:11:45 +0200

    implemented: basic image splitting

--- Image-Steganography/ImageSplitter.py
+++ Image-Steganography/ImageSplitter.py
@@ -4,9 +4,32 @@ import numpy as np
 
 
 class ImageSplitter(IImageMorphing.IImageMorphing):
-    def __init__(self, image):
+    def __init__(self, image, nrOfParts):
         super().__init__()
         self.image = image
+        self.nrOfParts = nrOfParts
+        self.parts = []
+
+        self.imageOperation()
 
     def imageOperation(self):
-        newSize = self.image.shape
+        height, width, channels = self.image.shape
+        """ how to divide the image into the required number of parts within a square format """
+        nrOfSquares = self.nrOfParts // 2
+        imgPartHeight = height // nrOfSquares
+        imgPartWidth = width // nrOfSquares
+
+        k = 1
+        for i in range(0, width // imgPartWidth):
+            for j in range(0, height // imgPartHeight):
+                """ 1/nrOfParts part of the original image """
+                newImgPart = self.image[j * imgPartHeight: j * imgPartHeight + imgPartHeight,
+                             i * imgPartWidth: i * imgPartWidth + imgPartWidth]
+
+                """ set the first Green pixel to the image part's order """
+                newImgPart.itemset((0, 0, 1), k)
+                self.parts.append(newImgPart)
+                k += 1
+
+    def getParts(self):
+        return self.parts
--- Image-Steganography/imageStega.py
+++ Image-Steganography/imageStega.py
@@ -8,30 +8,16 @@ import numpy as np
 
 
 def test():
-    image = cv2.imread("/home/czimbortibor/inputImage.jpg")
+    image = cv2.imread("/path/to/image")
     image = cv2.resize(image, (800, 600))
     cv2.imshow("window", image)
 
-    height, width, channels = image.shape
-    """ how many parts should there be """
     nrOfParts = 4
-    """ how to divide the image into the required number of parts within in a square format """
-    nrOfSquares = nrOfParts // 2
-    imgPartHeight = height // nrOfSquares
-    imgPartWidth = width // nrOfSquares
-
-    images = []
-    for i in range(0, width // imgPartWidth):
-        for j in range(0, height // imgPartHeight):
-            """ 1/nrOfParts part of the original image """
-            newImage = image[j * imgPartHeight: j * imgPartHeight + imgPartHeight, i * imgPartWidth: i * imgPartWidth \
-                                                                                                     + imgPartWidth]
-            images.append(newImage)
-
-    for i in range(0, len(images)):
-        cv2.imshow("window" + str(i), images[i])
+    imageSplitter = ImageSplitter.ImageSplitter(image, nrOfParts)
+    parts = imageSplitter.getParts()
+    for i in range(0, len(parts)):
+        cv2.imshow("part " + str(i + 1), parts[i])
 
     cv2.waitKey(0)
-
 
 test()
