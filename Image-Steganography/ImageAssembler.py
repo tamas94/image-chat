f4e27c9c5987e436cffe649bc2d31b547af6d013
czimbortibor <czimbortibor@gmail.com>
2016-11-22 22:27:39 +0200

    implemented: basic image assembling

--- Image-Steganography/ImageAssembler.py
+++ Image-Steganography/ImageAssembler.py
@@ -1,9 +1,40 @@
+import math
+
+import numpy as np
+
 import IImageMorphing
 
 
 class ImageAssembler(IImageMorphing.IImageMorphing):
-    def __init__(self):
+    def __init__(self, parts):
         super().__init__()
+        self.parts = parts
+        """ size of the 2 dimensional array which will contain all of the image parts """
+        self.sizeOfM = int(round(math.sqrt(len(parts)), 1))
+        self.fullImage = []
+
+        self.imageOperation()
 
     def imageOperation(self):
-        pass
+        """ 2D list for the image parts """
+        partsM = []
+        for i in range(0, self.sizeOfM):
+            partsM.append([])
+            for j in range(0, self.sizeOfM):
+                partsM[i].append(self.parts[i * self.sizeOfM + j])
+
+        """ stitch together the first row """
+        self.fullImage = partsM[0][0]
+        for i in range(1, self.sizeOfM):
+            self.fullImage = np.concatenate((self.fullImage, partsM[0][1]), axis=1)
+
+        """ stitch the rows starting from the 2nd one """
+        for i in range(1, self.sizeOfM):
+            imageRow = partsM[i][0]
+            for j in range(1, self.sizeOfM):
+                imageRow = np.concatenate((imageRow, partsM[i][j]), axis=1)
+            """ after a row is completed attach it below the previously stitched together rows """
+            self.fullImage = np.concatenate((self.fullImage, imageRow), axis=0)
+
+    def getImage(self):
+        return self.fullImage
--- Image-Steganography/ImageSplitter.py
+++ Image-Steganography/ImageSplitter.py
@@ -1,6 +1,6 @@
+import random
+
 import IImageMorphing
-import cv2
-import numpy as np
 
 
 class ImageSplitter(IImageMorphing.IImageMorphing):
@@ -19,6 +19,7 @@ class ImageSplitter(IImageMorphing.IImageMorphing):
         imgPartHeight = height // nrOfSquares
         imgPartWidth = width // nrOfSquares
 
+        random.seed()
         k = 1
         for i in range(0, width // imgPartWidth):
             for j in range(0, height // imgPartHeight):
@@ -28,8 +29,14 @@ class ImageSplitter(IImageMorphing.IImageMorphing):
 
                 """ set the first Green pixel to the image part's order """
                 newImgPart.itemset((0, 0, 1), k)
+
                 self.parts.append(newImgPart)
                 k += 1
+
+                """ save the image part with a random name """
+                x = random.randint(1, 9999999)
+                outputFile = str(x) + ".jpg"
+                # cv2.imwrite(outputFile, newImgPart)
 
     def getParts(self):
         return self.parts
