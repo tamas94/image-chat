0c6257c7133d4247831acc9b70d9664703ee0308
czimbortibor <czimbortibor@gmail.com>
2016-11-22 17:49:50 +0200

    merge changes

--- Image-Steganography/imageStega.py
+++ Image-Steganography/imageStega.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 def test():
-    image = cv2.imread("/path/to/image")
+    image = cv2.imread("/home/czimbortibor/inputImage.jpg")
     image = cv2.resize(image, (800, 600))
     cv2.imshow("window", image)
 
@@ -16,7 +16,7 @@ def test():
     imageSplitter = ImageSplitter.ImageSplitter(image, nrOfParts)
     parts = imageSplitter.getParts()
     for i in range(0, len(parts)):
-        cv2.imshow("part " + str(i + 1), parts[i])
+        cv2.imshow("part " + str(i+1), parts[i])
 
     cv2.waitKey(0)
 
