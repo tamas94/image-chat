1b2014204479fadd4e22e25206e72befe5a06094
czimbortibor <czimbortibor@gmail.com>
2016-11-30 21:17:18 +0200

    added README description

--- Image-Steganography/imageStega.py
+++ Image-Steganography/imageStega.py
@@ -5,18 +5,18 @@ import ImageSplitter
 
 
 def test():
-    image = cv2.imread("/home/czimbortibor/inputImage.jpg")
+    image = cv2.imread("path/to/image")
     image = cv2.resize(image, (800, 600))
     cv2.imshow("original", image)
 
     nrOfParts = 4
     imageSplitter = ImageSplitter.ImageSplitter(image, nrOfParts)
     parts = imageSplitter.getParts()
-    # for i in range(0, len(parts)):
-    #   cv2.imshow("part " + str(i+1), parts[i])
+
     imageAssembler = ImageAssembler.ImageAssembler(parts)
     fullImage = imageAssembler.getImage()
     cv2.imshow("reconstructed", fullImage)
     cv2.waitKey(0)
+
 
 test()
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # image-chat
+
+The purpose of this script:
+    Make the given text message encrypted in a visual manner. The user feed's the message for the script and selects
+    a suitable image (one which could hold the information). The output will be visually the same selected image
+    but with the message hidden inside of it.
+
+//TODO
+/** flexibility, which compression formats work */
+
+Image Steganography Overview:
+    http://repository.root-me.org/St%C3%A9ganographie/EN%20-%20Image%20Steganography%20Overview.pdf
