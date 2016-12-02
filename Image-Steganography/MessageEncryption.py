a7e245a5bafe96e35e34e8d8ed907298f357f615
czimbortibor <czimbortibor@gmail.com>
2016-12-02 18:01:04 +0200

    implemented the image assembling

--- Image-Steganography/MessageEncryption.py
+++ Image-Steganography/MessageEncryption.py
@@ -1,9 +1,26 @@
 import IMessageMorphing
+import numpy as np
 
 
 class MessageEncryption(IMessageMorphing.IMessageMorphing):
-    def __init__(self):
+    def __init__(self, message, image):
         super().__init__()
+        self.message = message
+        self.image = image
+
+        self.messageOperation()
 
     def messageOperation(self):
+        imageSize = np.size(self.image) // 8
+        print("image size:", imageSize, "bytes")
+        textSize = len(self.message)
+        print("message size:", textSize, "bytes")
+
+        if textSize > imageSize:
+            print("message does not fit in the image!")
+            exit()
+
+        self.LSB()
+
+    def LSB(self):
         pass
--- Image-Steganography/imageStega.py
+++ Image-Steganography/imageStega.py
@@ -1,13 +1,13 @@
-import cv2
-
 import ImageAssembler
 import ImageSplitter
+import cv2
 
 
 def test():
-    image = cv2.imread("path/to/image")
+    image = cv2.imread("/path/to/image")
     image = cv2.resize(image, (800, 600))
     cv2.imshow("original", image)
+
 
     nrOfParts = 4
     imageSplitter = ImageSplitter.ImageSplitter(image, nrOfParts)
@@ -17,6 +17,8 @@ def test():
     fullImage = imageAssembler.getImage()
     cv2.imshow("reconstructed", fullImage)
     cv2.waitKey(0)
-
-
+    '''
+    message = "Hello world!"
+    messageEncryptor = MessageEncryption.MessageEncryption(message, image)
+    '''
 test()
